863f3f6466ed3c914039b9f84c9fbb828ce44c79
Viet^^ <hoangvietdo.bk@gmail.com>
2024-03-26 16:02:39 +0900

    update files

--- EKF-VINS/INS.m
+++ EKF-VINS/INS.m
@@ -1,3 +1,25 @@
+% The MIT License
+% 
+% Copyright (c) 2021 Hoang Viet Do
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
 function [A_n, current_V, current_P, current_q, current_omega] = INS(f_hat, gyro_hat, dt, past_omega, past_q, g_n, past_V, past_P)
 %Intergrate quat
 w_x_hat = gyro_hat(1, 1);
